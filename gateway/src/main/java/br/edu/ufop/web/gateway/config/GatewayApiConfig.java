5365af972ec8c8ce26329733a99b177d8e999f5f
sakikout <bedalfior@gmail.com>
2026-02-10 13:36:05 -0300

    feat: add gateway

---
+++ gateway/src/main/java/br/edu/ufop/web/gateway/config/GatewayApiConfig.java
@@ -0,0 +1,53 @@
+package br.edu.ufop.web.gateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayApiConfig {
+
+    @Value("${gateway.frontend.uri}")
+    private String frontEndUri;
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("tickets",
+                        pred -> pred.path("/events/**", "/sales/**", "/users/**")
+                                .uri("http://tickets-app:4000")
+                )
+                .route("frontend",
+                        pred -> pred.path("/**")
+                                .uri(getFrontEndUri())
+                )
+                .build();
+    }
+
+    private String getFrontEndUri() {
+
+        final String FRONTEND_DEFAULT_URI = "http://react-app:3000";
+
+        if (this.frontEndUri != null) {
+            return this.frontEndUri;
+        }
+
+        return FRONTEND_DEFAULT_URI;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

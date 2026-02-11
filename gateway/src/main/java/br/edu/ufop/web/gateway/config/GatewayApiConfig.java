117b78c418bb4a134fc3c3f0cb1f44cbd83fc206
sakikout <bedalfior@gmail.com>
2026-02-11 18:52:21 -0300

    feat: add cors

--- gateway/src/main/java/br/edu/ufop/web/gateway/config/GatewayApiConfig.java
+++ gateway/src/main/java/br/edu/ufop/web/gateway/config/GatewayApiConfig.java
@@ -5,6 +5,11 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class GatewayApiConfig {
@@ -38,16 +43,24 @@ public class GatewayApiConfig {
 
     }
 
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        
+        corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        
+    
+        corsConfig.setMaxAge(3600L);
+        corsConfig.addAllowedMethod("*");
+        
+        corsConfig.addAllowedHeader("*");
+        
+        corsConfig.setAllowCredentials(true);
 
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
 
-
-
-
-
-
-
-
-
-
+        return new CorsWebFilter(source);
+    }
 
 }
